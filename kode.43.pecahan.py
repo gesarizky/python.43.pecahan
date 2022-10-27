d4eeb3ac4aa20cb120277108852bece76db6b898
Gesa Rizky Nugraha <109656432+gesarizky@users.noreply.github.com>
2022-10-27 14:22:39 +0700

    Add files via upload

---
+++ kode.43.pecahan.py
@@ -0,0 +1,18 @@
+# Author  : Gesa Rizky Nugraha
+# Email   : gesarizkynugraha@gmail.com
+# Website : karenabelajar.blogspot.com
+
+# Menginput
+print('Masukan Nilai dari a b/c')
+a = float(input("Tuliskan Nilai a : "))
+b = float(input("Tuliskan Nilai b: "))
+c = float(input("Tuliskan Nilai c: "))
+
+# Mengkonversi
+a1 = a*c+b
+
+# Menampilkan Hasil
+print('==========================================================')
+print('Maka Dari Pecahan %d %d/%d' %(a,b,c))
+print('Maka Menjadi Pecahan  %d/%d'  %(a1,c))
+print('==========================================================')
